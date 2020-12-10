1febbf8ee5e6d8f0b834b7f0160b50d01bb269af
tonic523 <59171113+tonic523@users.noreply.github.com>
2020-12-10 18:12:07 +0900

    Create Step-3
    루빅스 큐브 구현 프로젝트 생성 / 큐브 상태 출력과 입력 값을 명령어로 분류하는 메소드 구현

---
+++ src/masters_test/Step_3.java
@@ -0,0 +1,98 @@
+package masters_test;
+
+import java.util.Scanner;
+
+public class Step_3 {
+	static final int B = 0, W = 1, O = 2, G = 3, Y = 4, R = 5;
+
+	static char[][][] initCube(char cube[][][]) {
+		for (int j = 0; j < 3; j++) {
+			for (int j2 = 0; j2 < 3; j2++) {
+				cube[B][j][j2] = 'B';
+				cube[W][j][j2] = 'W';
+				cube[O][j][j2] = 'O';
+				cube[G][j][j2] = 'G';
+				cube[Y][j][j2] = 'Y';
+				cube[R][j][j2] = 'R';
+			}
+		}
+		return cube;
+	}
+
+	static void rotate(int f, int r, int u, int l, int d, boolean D) {
+
+	}
+
+	static void print(char cube[][][]) {
+		for (int j = 0; j < 3; j++) {
+			System.out.print("      ");
+			for (int j2 = 0; j2 < 3; j2++) {
+				System.out.print(cube[0][j][j2]);
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 1; j < 5; j++) {
+				for (int j2 = 0; j2 < 3; j2++) {
+					System.out.print(cube[j][i][j2]);
+				}
+				System.out.print("   ");
+			}
+			System.out.println();
+		}
+		for (int j = 0; j < 3; j++) {
+			System.out.print("      ");
+			for (int j2 = 0; j2 < 3; j2++) {
+				System.out.print(cube[5][j][j2]);
+			}
+			System.out.println();
+		}
+	}
+
+	static String[] getCommand(String input) {
+		String[] inputSplit = input.split("(?!^)");
+		String[] command = new String[inputSplit.length];
+		for (int i = 0; i < command.length; i++) {
+			if (i == command.length - 1) {
+				command[i] = inputSplit[i];
+			} else {
+				if (inputSplit[i + 1].equals("'")) {
+					command[i] = inputSplit[i] + inputSplit[i + 1];
+				} else {
+					command[i] = inputSplit[i];
+				}
+			}
+		}
+		return command;
+	}
+
+	public static void main(String[] args) {
+		char[][][] cube = new char[6][3][3];
+		int n = 0;
+		cube = initCube(cube);
+		print(cube);
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.printf("CUBE> ");
+			String input = scanner.nextLine();
+			String[] command = getCommand(input);
+			if (command[0].equals("Q")) {
+				System.out.println("조작갯수: " + n);
+				System.out.println("이용해주셔서 감사합니다!");
+				break;
+			}
+			for (int i = 0; i < command.length; i++) {
+				if (!command[i].equals("'")) {
+					System.out.println(command[i]);
+					//큐브 동작 구현
+					print(cube);
+					System.out.println();
+					n++;
+				}
+			}
+
+		}
+		scanner.close();
+	}
+
+}
